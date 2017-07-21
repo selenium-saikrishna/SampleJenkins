689516ee9c7310d6019b295c162f92e76c69631e
sai krishna <selenium.saikrishna@gmail.com>
2017-07-21 07:01:43 -0700

    Second commit

--- src/Example.java
+++ src/Example.java
@@ -1,3 +1,9 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Example {
 
@@ -5,7 +11,23 @@ public class Example {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Devops");
+		System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		//HtmlUnitDriver driver=new HtmlUnitDriver(BrowserVersion.CHROME);
+		driver.get("http://newtours.demoaut.com");
+		driver.findElement(By.linkText("REGISTER")).click();
+		WebElement drop=driver.findElement(By.name("country"));
+		List<WebElement> dropdown=drop.findElements(By.tagName("option"));
+		System.out.println(dropdown.size());
+		for(int i=0;i<dropdown.size();i++)
+		{
+			dropdown.get(i).click();
+			if(dropdown.get(i).isSelected())
+				System.out.println(dropdown.get(i).getText()+" is active");
+			else
+				System.out.println(dropdown.get(i).getText()+" is inactive");
+		}
+		driver.close();
 
 	}
 
